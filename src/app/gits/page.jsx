8f4c97a77605eb02e2c1e85a2af8317f519bc877
Elgius <114131653+Elgius@users.noreply.github.com>
2023-07-04 16:45:07 +0500

    updates 1

---
+++ src/app/gits/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GitCommands() {
+  return (
+    <div>
+      <h1 className="text-center text-4xl">GitCommands</h1>
+    </div>
+  );
+}
+
+export default GitCommands;
---
+++ src/app/linux/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LinuxCommands() {
+  return (
+    <div>
+      <h1 className="text-center text-4xl">LinuxCommands</h1>
+    </div>
+  );
+}
+
+export default LinuxCommands;
